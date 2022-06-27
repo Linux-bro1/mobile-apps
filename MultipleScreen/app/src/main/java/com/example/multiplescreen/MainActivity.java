4dda8639b425db293aa2291dc0953e66308bc19b
Amanur Rahman <aramancse@gmail.com>
2022-06-27 13:27:03 +0600

    Sign up page design

--- MultipleScreen/app/src/main/java/com/example/multiplescreen/MainActivity.java
+++ MultipleScreen/app/src/main/java/com/example/multiplescreen/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -15,8 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    EditText text;
-    TextView text1;
+    EditText editText;
+    TextView text;
     public static final String EXTRA_NAME ="com.example.multiplescreen.Extra";
 
     @Override
@@ -24,19 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text1 = findViewById(R.id.textView);
+        text = findViewById(R.id.textView);
 
 
         ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
-                Toast.makeText(MainActivity.this,"on activity", Toast.LENGTH_LONG).show();
                 if(result != null && result.getResultCode() == RESULT_OK){
-                    Toast.makeText(MainActivity.this,"result code", Toast.LENGTH_LONG).show();
                     if(result.getData() != null && result.getData().getStringExtra(Page2.USER) != null){
-                        Toast.makeText(MainActivity.this,"string set", Toast.LENGTH_LONG).show();
-
-                        text1.setText(result.getData().getStringExtra(Page2.USER));
+                        text.setText(result.getData().getStringExtra(Page2.USER));
+                        editText.setText("");
+                        editText.setHint("Enter your name");
                     }
                 }
             }
@@ -46,11 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this,Page2.class);
-            startForResult.launch(intent);
-            text = findViewById(R.id.editText);
-            String value = text.getText().toString();
+
+            editText = findViewById(R.id.editText);
+            String value = editText.getText().toString();
             intent.putExtra(EXTRA_NAME,value);
-            startActivity(intent);
+            startForResult.launch(intent);
         });
     }
 
--- MultipleScreen/app/src/main/java/com/example/multiplescreen/Page2.java
+++ MultipleScreen/app/src/main/java/com/example/multiplescreen/Page2.java
@@ -2,14 +2,17 @@ package com.example.multiplescreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Page2 extends AppCompatActivity {
 
-    TextView text;
+    EditText editText2;
+    TextView text2;
     Button button2;
     public static final String USER = "com.example.multiplescreen.USER";
     @Override
@@ -17,27 +20,35 @@ public class Page2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
-        text = findViewById(R.id.textView2);
+        editText2 = findViewById(R.id.editText2);
+        text2 = findViewById(R.id.textView2);
         button2 = findViewById(R.id.button2);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_NAME);
-        text.setText(message);
+        Intent resultIntent = getIntent();
+        String result = resultIntent.getStringExtra(MainActivity.EXTRA_NAME);
+        text2.setText(result);
 
-        button2.setOnClickListener(view -> {
-            text = findViewById(R.id.textView2);
-            String name = text.getText().toString();
-            Intent i = new Intent(this,MainActivity.class);
-            i.putExtra(USER, name);
-            setResult(RESULT_OK, i);
-            finish();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText2.getText().toString();
+                Intent i = new Intent();
+                i.putExtra(USER, name);
+                setResult(RESULT_OK, i);
+                finish();
+            }
         });
 
-    }
-//
 
-    @Override
-    public void onBackPressed() {
+
+//        if (editText2.getText().toString().equals("")) {
+//            Bundle bundle1 = getIntent().getExtras();
+//            if (bundle1 != null) {
+//                Toast.makeText(this,"Hit bundle again",Toast.LENGTH_SHORT).show();
+//                String message = bundle1.getString(MainActivity.EXTRA_NAME);
+//                text2.setText(message);
+//            }
+//        }
 
     }
 }
