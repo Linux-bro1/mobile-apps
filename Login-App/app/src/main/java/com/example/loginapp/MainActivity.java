9cf61a72ab4845427b390143aea44200947c508d
Amanur Rahman <aramancse@gmail.com>
2022-06-27 14:04:10 +0600

    home activity added

--- Login-App/app/src/main/java/com/example/loginapp/MainActivity.java
+++ Login-App/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username,email,password;
+    Button signup,signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        signin = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
